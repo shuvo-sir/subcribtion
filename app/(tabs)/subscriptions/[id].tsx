b1e572450ef6f62e6572fbca493d6ef44bdae301
Shuvo Halder <shuvohalder499@gmail.com>
2026-04-06 12:54:37 +0600

    add some file and route

---
+++ app/(tabs)/subscriptions/[id].tsx
@@ -0,0 +1,15 @@
+import { Link, useLocalSearchParams } from "expo-router";
+import React from "react";
+import { Text, View } from "react-native";
+
+const SubscriptionsDetails = () => {
+  const { id } = useLocalSearchParams<{ id: string }>();
+  return (
+    <View>
+      <Text>SubscriptionsDetails: {id}</Text>
+      <Link href={"/index"}>Go back</Link>
+    </View>
+  );
+};
+
+export default SubscriptionsDetails;
