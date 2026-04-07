de16ab88c897c25ebecc36d35306ff89767f7958
Shuvo Halder <shuvohalder499@gmail.com>
2026-04-07 17:10:48 +0600

    define the tab layout for the button tab navigation

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -3,6 +3,10 @@ import { Tabs } from "expo-router";
 const TabLayout = () => (
   <Tabs screenOptions={{ headerShown: false }}>
     <Tabs.Screen name="index" options={{ title: "Home" }} />
+    <Tabs.Screen name="subscriptions" options={{ title: "Subscriptions" }} />
+    <Tabs.Screen name="insights" options={{ title: "Insights" }} />
+    <Tabs.Screen name="settings" options={{ title: "Settings" }} />
+    <Tabs.Screen name="subscription/[id]" options={{ href: null }} />
   </Tabs>
 );
 
