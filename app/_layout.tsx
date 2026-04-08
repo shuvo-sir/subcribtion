3f087cf83840b5df1c569ec35c9de4710af68d39
Shuvo Halder <shuvohalder499@gmail.com>
2026-04-08 12:30:56 +0600

    Add some font

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,16 @@
 import "@/global.css";
+import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
+  const [fontLoaded] = useFonts({
+    "sans-regular": require("../assets/fonts/PlusJakartaSans-Regular.ttf"),
+    "sans-medium": require("../assets/fonts/PlusJakartaSans-Medium.ttf"),
+    "sans-semibold": require("../assets/fonts/PlusJakartaSans-SemiBold.ttf"),
+    "sans-bold": require("../assets/fonts/PlusJakartaSans-Bold.ttf"),
+    "sans-extrabold": require("../assets/fonts/PlusJakartaSans-ExtraBold.ttf"),
+    "sans-light": require("../assets/fonts/PlusJakartaSans-Light.ttf"),
+  });
   return (
     <Stack
       // This tells the app to land on (tabs) by default
